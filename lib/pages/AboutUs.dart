d1274f3574166dc522db421a564c946d1be42277
Nirav <NiravPatel@Rahuls-MacBook-Pro.local>
2019-08-17 11:37:54 +0530

    Fix about us page issue

--- lib/pages/AboutUs.dart
+++ lib/pages/AboutUs.dart
@@ -17,95 +17,97 @@ class AboutUs extends StatelessWidget {
           BackGroundImage(
             image: "assets/images/image1.jpg",
           ),
-          Column(
-            children: <Widget>[
-              Card(
-                margin: EdgeInsets.all(20.0),
-                child: Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "About Us",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18.0),
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ")
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.all(20.0),
-                child: Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Our Gallery",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18.0),
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Container(
-                        height: 150.0,
-                        child: ListView.separated(
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index) {
-                            return ClipRRect(
-                              borderRadius: BorderRadius.circular(15.0),
-                              child: Container(
-                                child: Image.asset("assets/images/image1.jpg"),
-                              ),
-                            );
-                          },
-                          separatorBuilder: (context, index) {
-                            return SizedBox(
-                              width: 10.0,
-                            );
-                          },
-                          itemCount: 10,
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Card(
+                  margin: EdgeInsets.all(20.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "About Us",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18.0),
                         ),
-                      )
-                    ],
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ")
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                margin: EdgeInsets.all(20.0),
-                child: Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Our Story",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18.0),
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ")
-                    ],
+                Card(
+                  margin: EdgeInsets.all(20.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Our Gallery",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18.0),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Container(
+                          height: 150.0,
+                          child: ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return ClipRRect(
+                                borderRadius: BorderRadius.circular(15.0),
+                                child: Container(
+                                  child: Image.asset("assets/images/image1.jpg"),
+                                ),
+                              );
+                            },
+                            separatorBuilder: (context, index) {
+                              return SizedBox(
+                                width: 10.0,
+                              );
+                            },
+                            itemCount: 10,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Card(
+                  margin: EdgeInsets.all(20.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Our Story",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18.0),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ")
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
